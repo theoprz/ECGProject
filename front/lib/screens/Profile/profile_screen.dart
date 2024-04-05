bf4c8f781ab1faa2cd301f0da0dba52713fc705c
Aurélien ROGÉ <72409083+AurelienRoge@users.noreply.github.com>
2024-04-05 15:45:07 +0200

    delete useless import

--- front/lib/screens/Profile/profile_screen.dart
+++ front/lib/screens/Profile/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'components/compte_screen.dart';
 import 'components/informations_screen.dart';
 import 'components/profile_menu.dart';
