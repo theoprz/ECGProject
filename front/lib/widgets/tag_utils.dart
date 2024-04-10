b03ff58a47acda20ab5f642669c445cf6f7aa290
Aurélien ROGÉ <72409083+AurelienRoge@users.noreply.github.com>
2024-04-10 16:24:50 +0200

    DE NOMBREUX AJOUTS ET AMÉLIORATIONS SELECTION TAGS
    -Pas de doublon dans la liste des tags
    -Cliquer sur un tag le supprime
    -Bouton flottant pour valider la sélection
    -Bouton pour valider un tag parent sans avoir à choisir d'enfant
    -Petit séparateur entre chaque tag
    -BUGFIX : modification du == sur la classe TagNode pour permettre la comparaison et donc éviter les doublons
    -BUGFIX : Suppression des tags avec nom ou id vide
    -Dernier todo en fin de fichier en attendant le lien avec back

--- front/lib/widgets/tag_utils.dart
+++ front/lib/widgets/tag_utils.dart
@@ -24,39 +24,46 @@ List<TagNode> selectTagAndAncestors(TagNode node) {
 
 
 Widget buildTagNode(BuildContext context, TagNode node) {
-  return ListTile(
-    title: Text('${node.tag.name}'),
-    subtitle: Text('ID: ${node.tag.id}, Parent ID: ${node.tag.parentId}'),
-    trailing: node.children.isNotEmpty ? Icon(Icons.arrow_forward_ios) : null,
-    onTap: () {
-      if (node.children.isEmpty) {
-        List<TagNode> selectedTags = selectTagAndAncestors(node);
+  return Container(
+      decoration: BoxDecoration(
+      border: Border(
+      bottom: BorderSide(width: 1, color: Colors.grey.shade300), // Ajoute une bordure en bas
+      ),
+    ),
+    child:ListTile(
+      title: Text('${node.tag.name}'),
+      subtitle: Text('ID: ${node.tag.id}, Parent ID: ${node.tag.parentId}'),
+      trailing: node.children.isNotEmpty ? Icon(Icons.arrow_forward_ios) : null,
+      onTap: () {
+        if (node.children.isEmpty) {
+          List<TagNode> selectedTags = selectTagAndAncestors(node);
 
-        //Add selected tags to globalSelectedTags if they are not already in the list
-        for (TagNode tag in selectedTags) {
-          if (!globalSelectedTags.contains(tag)) {
-            globalSelectedTags.add(tag);
+          //Add selected tags to globalSelectedTags if they are not already in the list
+          for (TagNode tag in selectedTags) {
+            if (!globalSelectedTags.contains(tag)) {
+              globalSelectedTags.add(tag);
+            }
           }
+
+          //2nd step verification to avoid duplicates
+          globalSelectedTags = globalSelectedTags.toSet().toList();
+
+          globalSelectedTagsController.add(globalSelectedTags);
+
+          // Retour à l'écran des tags racine
+          for(int i = 0; i < selectedTags.length -1; i++){
+          Navigator.pop(context);
         }
 
-        //2nd step verification to avoid duplicates
-        globalSelectedTags = globalSelectedTags.toSet().toList();
-
-        globalSelectedTagsController.add(globalSelectedTags);
-
-        // Retour à l'écran des tags racine
-        for(int i = 0; i < selectedTags.length -1; i++){
-        Navigator.pop(context);
-      }
-
-      } else
-      if (node.children.isNotEmpty) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ChildTagSelectionPage(parent: node)),
-        );
-      }
-    },
+        } else
+        if (node.children.isNotEmpty) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ChildTagSelectionPage(parent: node)),
+          );
+        }
+      },
+    )
   );
 }
 
